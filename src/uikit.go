58db5dd9ef6d36f7d0d6fe70c9a67a5990a21003
havice <dnm@project-igenoki.info>
2015-05-29 19:28:24 +0900

    文字は出せるが位置が変更できない

--- src/uikit.go
+++ src/uikit.go
@@ -1,7 +1,11 @@
 package uikit
 
 import (
+	"io/ioutil"
+	"log"
+
 	"github.com/google/gxui"
+	"github.com/google/gxui/samples/flags"
 	"github.com/google/gxui/themes/dark"
 )
 
@@ -9,9 +13,35 @@ const (
 	windowName string = "test window"
 )
 
+func getFont(name string) []byte {
+	//file, err := os.Open("../font/ + name")
+	file, err := ioutil.ReadFile("../font/" + name)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return file
+}
+
 func AppMain(driver gxui.Driver) {
+
 	theme := dark.CreateTheme(driver)
+	font, err := driver.CreateFont(getFont("VL-Gothic-Regular.ttf"), 75)
+
 	window := theme.CreateWindow(1920, 1080, windowName)
+	//canvas := driver.CreateCanvas(math.Size{W: 1920, H: 1080})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	label := theme.CreateLabel()
+
+	label.SetFont(font)
+	label.SetText("本日は晴天なり")
+
+	//label.SetSize(math.Size{H: 10000, W: 10000})
+
+	window.SetScale(flags.DefaultScaleFactor)
 
 	window.OnClose(driver.Terminate)
 }
